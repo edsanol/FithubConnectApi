159267a4cf807f4ec76e7f32d9322072f5de9cab
Edinael Sanguino Olivares <edsanol99@gmail.com>
2024-11-11 15:11:14 -0500

    Refactor Swagger setup to always enable in all environments

--- Api/Program.cs
+++ Api/Program.cs
@@ -71,11 +71,13 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 app.MapControllers();
